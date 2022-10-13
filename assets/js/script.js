64556eba831cbbc45579e0f7a6d9403add7261be
Marcialas <martynaspalsys@gmail.com>
2022-10-13 17:44:39 +0000

    Add division game mode and now its finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,6 +39,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === 'subtract') {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === 'division') {
+        displayDivideQuestion(num1, num2);
     }
     else {
         alert(`Unknown game type: ${gameType}`);
@@ -83,6 +85,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === '/') {
+        return [operand1 / operand2, 'division'];
     }
     else {
         alert(`Unimplemented operator ${operator}`);
@@ -135,5 +139,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 
 function displayDivideQuestion(operand1, operand2) {
-
+    document.getElementById("operand1").textContent = operand1 * operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
 }
